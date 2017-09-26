f18f0a864b75f5eaf4d4f1c6000d7453e70a5dd5
chenqinghe <chenqh721@foxmail.com>
2017-09-26 13:57:50 +0800

    filewatcher beta1

--- vendor/Swolf/src/Component/Process/FileWatcher.php
+++ vendor/Swolf/src/Component/Process/FileWatcher.php
@@ -15,13 +15,16 @@ class FileWatcher implements Process
 {
 
 
-    protected $useInotify;
+    protected $useInotify = false;
 
 
     protected $inotifyInstance;
 
 
-    protected $watchDescriptor;
+    protected $watchDescriptor = [];
+
+
+    protected $watchTarget = [];
 
 
     public function __construct($useInotify = false)
@@ -34,17 +37,16 @@ class FileWatcher implements Process
 
     public function addWatchTarget($pathname)
     {
+
+        $target = new WatchTarget($pathname);
+
         if ($this->useInotify) {
-            $watchDescriptor = inotify_add_watch($this->inotifyInstance, $pathname, IN_CREATE | IN_MODIFY | IN_DELETE);
-            $this->watchDescriptor[] = $watchDescriptor;
+            $watchDescriptor = inotify_add_watch($this->inotifyInstance, $target, IN_CREATE | IN_MODIFY | IN_DELETE);
+            $this->watchDescriptor[$target] = $watchDescriptor;
+            return;
         }
 
-
-    }
-
-
-    protected function getAbsolutePath($path)
-    {
+        $this->watchTarget[$target] = $target;
 
     }
 
@@ -52,6 +54,16 @@ class FileWatcher implements Process
     public function removeWatchTarget($pathname)
     {
 
+        $target = new WatchTarget($pathname);
+
+        if ($this->useInotify) {
+            if (!inotify_rm_watch($this->inotifyInstance, $this->watchDescriptor[$target])) {
+                throw new \Exception('cannot remove target from target list.');
+            }
+            return true;
+        }
+        unset($this->watchTarget[$target]);
+        return true;
     }
 
     public function watch()
@@ -67,7 +79,7 @@ class FileWatcher implements Process
 
         while (true) {
 
-            if ($this->isChanged()) {
+            if ($this->isTargetChanged()) {
                 Resource::$server->reload();
             }
             sleep(1);
@@ -75,8 +87,14 @@ class FileWatcher implements Process
     }
 
 
-    protected function isChanged()
+    protected function isTargetChanged()
     {
+        foreach ($this->watchTarget as $k => $v) {
+            if ($v->hash() !== $v->hashResult) {
+                return true;
+            }
+        }
+        return false;
 
     }
 
@@ -85,4 +103,61 @@ class FileWatcher implements Process
     {
         $this->watch();
     }
+}
+
+
+class WatchTarget
+{
+
+    private $absPath;
+
+
+    public $hashResult = '';
+
+
+    public function __construct($path)
+    {
+        $absPath = realpath($path);
+        if ($absPath === false) {
+            throw new \Exception('the file or directory is not exists.');
+        }
+        $this->absPath = $absPath;
+    }
+
+
+    public function isChanged()
+    {
+        static $hashResult;
+        $hr = $this->hash();
+        $equal = $hr == $hashResult;
+        $hashResult = $hr;
+        return $equal;
+    }
+
+
+    public function hash()
+    {
+        $files = scandir($this->absPath);
+        $md5str = '';
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                $t = new WatchTarget($file);
+                $md5str .= $t->hash();
+            } else {
+
+                $md5str .= md5_file($file);
+            }
+        }
+
+        return md5($md5str);
+
+    }
+
+
+    public function __toString()
+    {
+        return $this->absPath;
+    }
+
+
 }
