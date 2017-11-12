c94438249879e283a94c5a6003a3964dbbea8169
chenqinghe <chenqh721@foxmail.com>
2017-11-12 23:04:58 +0800

    move app.php from application dir to root dir

--- app.php
+++ app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Config\Handler;
 use App\Config\Server;
@@ -8,12 +8,28 @@ use App\Config\Process;
 use Swolf\Core\Server\HttpServer;
 use Swolf\Core\Server\WebsocketServer;
 use Swolf\Core\Server\BasicServer;
-use Swolf\Component\Command\Parser;
+use Inhere\Console\IO\Input;
+use Inhere\Console\IO\Output;
+use Inhere\Console\Application;
 
-$command = new Parser();
-$host = &$command->String('host', Parser::PROVIDE_OPTIONAL, '0.0.0.0', 'the host listen to');
-$port = &$command->Int('port', Parser::PROVIDE_OPTIONAL, '9501', 'the port listen to');
-$command->Parse();
+$meta = [
+    'name' => 'My Console App',
+    'version' => '1.0.2',
+];
+$input = new Input;
+$output = new Output;
+$app = new Application($meta, $input, $output);
+
+// add command routes
+$app->command('demo', function (Input $in, Output $out) {
+    $cmd = $in->getCommand();
+
+    $out->info('hello, this is a test command: ' . $cmd);
+});
+// ... ...
+
+// run
+$app->run();
 empty($host) && $host = Server::$setting['host'];
 empty($port) && $port = Server::$setting['port'];
 
